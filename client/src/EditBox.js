fc4fca6a3c5e749609366ef2dcf07b4c907b7dcd
Max <max.edison@yahoo.com>
2022-01-15 12:06:21 -0500

    configured editor

---
+++ client/src/EditBox.js
@@ -0,0 +1,77 @@
+import {EditorState, convertToRaw} from 'draft-js'
+import { Editor } from 'react-draft-wysiwyg';
+import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import {useState} from 'react'
+
+function EditBox(){
+
+    const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+
+
+    return(
+        <Editor
+        wrapperStyle={{border:"1px solid blue"}}
+        editorStyle={{border: "2px solid black"}}
+        toolbar={{
+          options: ['inline', 'fontSize', 'fontFamily', 'list', 'textAlign', 'emoji'],
+          inline: {
+            options: ['bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript'],
+            bold: { className: 'bordered-option-classname' },
+            italic: { className: 'bordered-option-classname' },
+            underline: { className: 'bordered-option-classname' },
+            strikethrough: { className: 'bordered-option-classname' },
+            superscript: { className: 'bordered-option-classname' },
+            subscript: { className: 'bordered-option-classname' },
+          },
+          fontSize: {
+            className: 'bordered-option-classname',
+          },
+          fontFamily: {
+            className: 'bordered-option-classname',
+          }, list: {
+            inDropdown: false,
+            className: undefined,
+            component: undefined,
+            dropdownClassName: undefined,
+            options: ['unordered', 'ordered', 'indent', 'outdent'],
+            unordered: { className: 'bordered-option-classname' },
+            ordered: { className: 'bordered-option-classname' },
+            indent: {className: 'bordered-option-classname' },
+            outdent: { className: 'bordered-option-classname' },
+          },
+          textAlign: {
+            inDropdown: false,
+            className: undefined,
+            component: undefined,
+            dropdownClassName: undefined,
+            options: ['left', 'center', 'right', 'justify'],
+            left: { className: 'bordered-option-classname' },
+            center: { className: 'bordered-option-classname' },
+            right: { className: 'bordered-option-classname' },
+            justify: { className: 'bordered-option-classname' },
+          },  
+          emoji: {
+            className: undefined,
+            component: undefined,
+            popupClassName: undefined,
+            emojis: [
+              '😀', '😁', '😂', '😃', '😉', '😋', '😎', '😍', '😗', '🤗', '🤔', '😣', '😫', '😴', '😌', '🤓',
+              '😛', '😜', '😠', '😇', '😷', '😈', '👻', '😺', '😸', '😹', '😻', '😼', '😽', '🙀', '🙈',
+              '🙉', '🙊', '👼', '👮', '🕵', '💂', '👳', '🎅', '👸', '👰', '👲', '🙍', '🙇', '🚶', '🏃', '💃',
+              '⛷', '🏂', '🏌', '🏄', '🚣', '🏊', '⛹', '🏋', '🚴', '👫', '💪', '👈', '👉', '👉', '👆', '🖕',
+              '👇', '🖖', '🤘', '🖐', '👌', '👍', '👎', '✊', '👊', '👏', '🙌', '🙏', '🐵', '🐶', '🐇', '🐥',
+              '🐸', '🐌', '🐛', '🐜', '🐝', '🍉', '🍄', '🍔', '🍤', '🍨', '🍪', '🎂', '🍰', '🍾', '🍷', '🍸',
+              '🍺', '🌍', '🚑', '⏰', '🌙', '🌝', '🌞', '⭐', '🌟', '🌠', '🌨', '🌩', '⛄', '🔥', '🎄', '🎈',
+              '🎉', '🎊', '🎁', '🎗', '🏀', '🏈', '🎲', '🔇', '🔈', '📣', '🔔', '🎵', '🎷', '💰', '🖊', '📅',
+              '✅', '❎', '💯',
+            ],
+          },
+        }}
+         editorState={editorState}
+         onEditorStateChange={setEditorState}
+                    />
+
+    )
+}
+
+export default EditBox;
--- client/src/EditorComponent.js
+++ client/src/EditorComponent.js
@@ -1,11 +1,15 @@
 import {useState} from 'react'
 import {EditorState, convertToRaw} from 'draft-js'
-import { Editor } from 'react-draft-wysiwyg';
+import EditBox from './EditBox'
 import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 
+
 function EditorComponent(){
+
+  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+
 
     const handleRecipeSubmit=(e)=>{
         // console.log("elloGuvnah!")
@@ -39,13 +43,17 @@ function EditorComponent(){
 
     return(
         <>
-        <form>
-        <TextField value={recipe.title} name="title" onChange={handleRecipeWrite}/>
+        {/* <TextField value={recipe.title} name="title" onChange={handleRecipeWrite}/>
         <TextField value={recipe.ingredients} name="ingredients" onChange={handleRecipeWrite}/>
         <TextField value={recipe.equipment} name="equipment" onChange={handleRecipeWrite}/>
-        <TextField value={recipe.instructions} name="instructions" onChange={handleRecipeWrite}/>
+        <TextField value={recipe.instructions} name="instructions" onChange={handleRecipeWrite}/> */}
+
+        <EditBox />
+        <EditBox />
+        <EditBox />
+        <EditBox />
+      
         <Button onClick={handleRecipeSubmit} variant="contained">CREATE!</Button>
-        </form>
         </>
     )
 }
